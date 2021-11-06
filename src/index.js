965ef057dbd0a3486e0adab930a65df0a20e8cf1
galiovets <m.galiovets@gmail.com>
2021-11-06 23:51:00 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+import debounce from 'lodash.debounce';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
 import './sass/main.scss';
+import fetchCountries from './js/fetchCountries';
+import countryListTempl from './templates/country-list.hbs';
+import countryCardTempl from './templates/country-card.hbs';
+
+const inputRef = document.querySelector('#js-input');
+const containerRef = document.querySelector('.countries');
+
+inputRef.addEventListener('input', debounce(searchCountry, 500));
+
+function searchCountry() {
+    fetchCountries(inputRef.value).then(data => {
+        if (!data.length) createMarkUp();
+        if (data.length === 1) createMarkUp(countryCardTempl(data));
+        if (data.length >= 2 && data.length <= 10) createMarkUp(countryListTempl(data));
+        if (data.length > 10) errMessage('Please enter more specific request');
+    });
+}
+
+function createMarkUp(markup) {
+    containerRef.innerHTML = "";
+    if (markup) containerRef.insertAdjacentHTML('beforeend', markup);
+}
+
+function errMessage(message) {
+    return error({
+      text: message,
+      delay: 1500,
+      closer: false,
+      title: 'Error',
+      icon: false,
+      width: '250px',
+      sticker: false,
+      addClass: 'error-box',
+    });
+  }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,4 @@
+export default function fetchCountries(searchQuery) {
+    return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+        .then(response => response.json());
+}
